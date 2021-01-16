a5a76475fa317420e6fa47e651392bfea30bc271
washifapsg <67793201+washifapsg@users.noreply.github.com>
2021-01-16 23:36:26 +0530

    Create minutes to years and days.java

---
+++ minutes to years and days.java
@@ -0,0 +1,15 @@
+package project;
+import java.util.*;
+public class Projecttwo {
+	
+	public static void main(String[] args) {
+		System.out.println("enter minutes");
+		Scanner sc =new Scanner(System.in);
+		float a= sc.nextInt();
+		float b=(float) (a/(60*24*365));
+		int c= (int) b;
+		float d=(b-c)*365;
+		int e= (int) d;
+		System.out.println(c+"year and "+ e +" days");
+		}
+}
